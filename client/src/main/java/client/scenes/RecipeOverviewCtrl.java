b8c71c65d48ecdf0ba0a9a37b4478e8ac6892e00
Eliott Duparc <e.j.duparc@student.tudelft.nl>
2025-11-25 10:39:20 +0100

    start creating main scene

---
+++ client/src/main/java/client/scenes/RecipeOverviewCtrl.java
@@ -0,0 +1,10 @@
+package client.scenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class RecipeOverviewCtrl {
+
+//    @FXML
+//    private ListView<Recipe> recipeList;
+}
