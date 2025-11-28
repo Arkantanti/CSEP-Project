0ee4c39c079cfff75e27fe11748c0a1f92d73932
Anando Hirschau <a.j.o.hirschau@student.tudelft.nl>
2025-11-28 18:58:14 +0100

    final fixes

--- server/src/main/java/server/database/SimpleRecipeRepository.java
+++ server/src/main/java/server/database/SimpleRecipeRepository.java
@@ -7,35 +7,35 @@ import java.util.Optional;
 
 public interface SimpleRecipeRepository {
     /**
-     *
-     * @return
+     * looks for everything in the database.
+     * @return all the items
      */
     List<Recipe> findAll();
 
     /**
-     *
-     * @param id
-     * @return
+     * function to find items with that id
+     * @param id checks for the id it needs to have
+     * @return the items with that id
      */
     Optional<Recipe> findById(Long id);
 
     /**
-     *
-     * @param id
-     * @return
+     * checks if that id exists
+     * @param id the id it checks for
+     * @return true or false depending on if the database has the id.
      */
     boolean existsById(Long id);
 
     /**
-     *
-     * @param recipe
-     * @return
+     * function to save the new value
+     * @param recipe the new recipe it needs to save
+     * @return saves the new value of the recipe to the database
      */
     Recipe save(Recipe recipe);
 
     /**
-     *
-     * @param id
+     * function to delete the id from the database
+     * @param id the id of the item it should delete from the database.
      */
     void deleteById(Long id);
 
