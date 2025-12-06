06562c67578de8b123af139c0a099d55b996aad7
Anando Hirschau <a.j.o.hirschau@student.tudelft.nl>
2025-12-06 16:15:27 +0100

    Faulty test caused pipeline to not pass

--- client/src/test/java/client/scenes/RecipeViewCtrlTest.java
+++ client/src/test/java/client/scenes/RecipeViewCtrlTest.java
@@ -12,11 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecipeViewCtrlTest {
 
+    private MainCtrl sut;
     private RecipeViewCtrl ctrl;
 
     @BeforeEach
     void setup() {
-        ctrl = new RecipeViewCtrl(null, null, null);
+        ctrl = new RecipeViewCtrl(null, sut = new MainCtrl(), null);
     }
 
     private String format(RecipeIngredient ri) {
