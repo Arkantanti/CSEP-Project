9c86ef342c1d718441fc07464f9e2e7af0e04de8
Anando Hirschau <a.j.o.hirschau@student.tudelft.nl>
2025-12-19 18:44:47 +0100

    Added some of the tests for the commons so I have enough lines of code.

--- commons/src/test/java/commons/IngredientTest.java
+++ commons/src/test/java/commons/IngredientTest.java
@@ -1,32 +1,44 @@
 package commons;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IngredientTest {
+    Ingredient ingredient;
+    Ingredient ingredient2;
 
-    @Test
-    void getId() {
+    @BeforeEach
+    void setUp(){
+        ingredient = new Ingredient("fat", 50, 24, 45);
+        ingredient2 = new Ingredient("fly", 50, 24, 45);
     }
 
     @Test
     void getName() {
+        assertEquals(ingredient.getName(), "fat");
     }
 
     @Test
     void getFat() {
+        assertEquals(ingredient.getFat(), 50);
     }
 
     @Test
     void getProtein() {
+        assertEquals(ingredient.getProtein(), 24);
     }
 
     @Test
     void getCarbs() {
+        assertEquals(ingredient.getCarbs(), 45);
+
     }
 
     @Test
     void setName() {
+        ingredient.setName("Hello");
+        assertEquals(ingredient, "Hello");
     }
 }
