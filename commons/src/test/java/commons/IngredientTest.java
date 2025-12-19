212d885c79de0f43c4838eeb371894d46ba1a3da
Anando Hirschau <a.j.o.hirschau@student.tudelft.nl>
2025-12-19 19:07:45 +0100

    These are the tests to make sure I have a hundred lines of code.

--- commons/src/test/java/commons/IngredientTest.java
+++ commons/src/test/java/commons/IngredientTest.java
@@ -8,37 +8,72 @@ import static org.junit.jupiter.api.Assertions.*;
 class IngredientTest {
     Ingredient ingredient;
     Ingredient ingredient2;
+    Ingredient ingredient3;
 
     @BeforeEach
     void setUp(){
         ingredient = new Ingredient("fat", 50, 24, 45);
         ingredient2 = new Ingredient("fly", 50, 24, 45);
+        ingredient3 = new Ingredient("fat", 50, 24, 45);
     }
 
     @Test
     void getName() {
-        assertEquals(ingredient.getName(), "fat");
+        assertEquals("fat", ingredient.getName());
     }
 
     @Test
     void getFat() {
-        assertEquals(ingredient.getFat(), 50);
+        assertEquals(50, ingredient.getFat());
     }
 
     @Test
     void getProtein() {
-        assertEquals(ingredient.getProtein(), 24);
+        assertEquals(24, ingredient.getProtein());
     }
 
     @Test
     void getCarbs() {
-        assertEquals(ingredient.getCarbs(), 45);
+        assertEquals(45, ingredient.getCarbs());
 
     }
 
     @Test
     void setName() {
         ingredient.setName("Hello");
-        assertEquals(ingredient, "Hello");
+        assertEquals("Hello", ingredient.getName());
+    }
+
+    @Test
+    void setFat() {
+        ingredient.setFat(40);
+        assertEquals(40, ingredient.getFat());
+    }
+
+    @Test
+    void setCarbs() {
+        ingredient.setCarbs(40);
+        assertEquals(40, ingredient.getCarbs());
+    }
+
+    @Test
+    void setProtein() {
+        ingredient.setProtein(40);
+        assertEquals(40, ingredient.getProtein());
+    }
+
+    @Test
+    void equalsTest(){
+        assertTrue(ingredient.equals(ingredient3));
+    }
+
+    @Test
+    void equalsFalseTest(){
+        assertFalse(ingredient.equals(ingredient2));
+    }
+
+    @Test
+    void equalsNullTest(){
+        assertFalse(ingredient.equals(null));
     }
 }
